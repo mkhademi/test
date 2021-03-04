1ed0e941d584004c8eae27faf89b21217c5fc508
Maryam Khademi <m.khademi@gmail.com>
2021-03-03 16:03:15 -0800

    burger

--- hungry.py
+++ hungry.py
@@ -2,6 +2,7 @@ hungry = input("are you hungry?")
 if hungry == "yes":
     print("eat apple")
     print("eat fries")
+    print("eat burger")
 else:
     thirsty = input("are you thirsty?")
     if thirsty == "yes":
